ebcc891e92ac959d4bf116c7e74cd970a545cf5b
hnineaindrakyaw <110731509+hnineaindrakyaw@users.noreply.github.com>
2022-10-20 02:06:09 +0630

    book

--- src/main/java/com/example/myapp/controller/BookController.java
+++ src/main/java/com/example/myapp/controller/BookController.java
@@ -17,12 +17,13 @@ public class BookController {
     private BookService bookService;
     @GetMapping("/")
     public  String index(Model model){
-      /*  model.addAttribute("category",new Category());
+       /*model.addAttribute("category",new Category());
         model.addAttribute("author",new Author());
         model.addAttribute("book",new Book());
         model.addAttribute("authors",bookService.findAllAuthors());
         model.addAttribute("categories",bookService.findAllCategory());
         model.addAttribute("books",bookService.findAllBoooks());*/
+
 
 
 
