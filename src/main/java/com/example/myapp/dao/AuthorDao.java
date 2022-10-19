85dad84f94b02a7813ed25b00680549b7089e543
hnineaindrakyaw <110731509+hnineaindrakyaw@users.noreply.github.com>
2022-10-19 15:00:39 +0630

    book

---
+++ src/main/java/com/example/myapp/dao/AuthorDao.java
@@ -0,0 +1,8 @@
+package com.example.myapp.dao;
+
+import com.example.myapp.ds.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorDao  extends JpaRepository<Author,Integer> {
+
+}
---
+++ src/main/java/com/example/myapp/service/BookService.java
@@ -0,0 +1,61 @@
+package com.example.myapp.service;
+
+import com.example.myapp.dao.AuthorDao;
+import com.example.myapp.dao.BookDao;
+import com.example.myapp.dao.CategoryDao;
+import com.example.myapp.ds.Author;
+import com.example.myapp.ds.Book;
+import com.example.myapp.ds.Category;
+import org.apache.el.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class BookService {
+    @Autowired
+    private AuthorDao authorDao;
+    @Autowired
+    private BookDao bookDao;
+    @Autowired
+    private CategoryDao categoryDao;
+    public  void saveAuthor(Author author){
+        authorDao.save(author);
+    }
+
+
+    public  void  saveCategory(Category category){
+        categoryDao.save(category);
+    }
+    public List<Category> findAllCategory(){
+        return categoryDao.findAll();
+    }
+    public Category findCategoryById(int id){
+        return  categoryDao.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+    public  List<Author> findAllAuthors(){
+        return authorDao.findAll();
+    }
+    @Transactional
+    public  void  saveBook(Book book,int categoryId,int authorId){
+
+
+        Category category=categoryDao.findById(categoryId).orElseThrow(EntityNotFoundException::new);
+        Author author=authorDao.findById(authorId).orElseThrow(EntityNotFoundException::new);
+        category.addBook(book);
+        author.addBook(book);
+        bookDao.save(book);
+
+
+    }
+    public  List<Book> findAllBoooks(){
+        return  bookDao.findAll();
+    }
+
+
+
+
+}
